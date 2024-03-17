74eacbab0c312ba939ac115b77fd1ae6d8e4e721
uberswe <m@rkus.io>
2024-03-17 13:50:37 +0100

    Various tweaks and fixes

--- server/kubejs/server_scripts/script.js
+++ server/kubejs/server_scripts/script.js
@@ -1,25 +1,31 @@
 let PlayersLastPos = {}
 
 PlayerEvents.tick(e => {
+	if(Utils.server.tickCount % 5) return
+
 	let { player } = e;
-	let playerUUID = player.getId();
-	let { x, z } = player
 	if (player.getLevel().getDimension() !== "uberswe:terra") {
 		return
 	}
+
+	let playerUUID = player.getId();
+	let { x, z } = player
 
 	let speed = 0
 	Object.keys(PlayersLastPos).forEach(key => {
 		if (key === playerUUID && PlayersLastPos[key] !== undefined && PlayersLastPos[key].x !== 0 && PlayersLastPos[key].z !== 0) {
 			speed = Math.sqrt( Math.pow((PlayersLastPos[key].x-x), 2) + Math.pow((PlayersLastPos[key].z-z), 2) );
+			let timediff = (new Date).getTime() - PlayersLastPos[key].lastTime
+			// divide time diff by 50 because 1 tick is 50 ms
+			speed = ((speed * 1000) / ((timediff) / 50)) / 1000
 		}
 	})
-	PlayersLastPos[playerUUID]= {x:x, z:z}
+	PlayersLastPos[playerUUID]= {x:x, z:z, lastTime: (new Date).getTime()}
 	if (speed > 5) {
 		// If speed is over 20 it's probably a teleport or something
-	} else if (speed > 0.8) {
+	} else if (speed > 0.6) {
 		//player.displayClientMessage(Component.gold('Speed: ').append(Component.red(speed.toFixed(2))), true)
-		player.displayClientMessage(Component.red('Moving Too Fast'), true)
-		player.attack('generic', 4)
+		player.displayClientMessage(Component.red('Moving Too Fast:').append(Component.red(speed.toFixed(2))), true)
+		player.attack('generic', 8)
 	}
 })
--- server/kubejs/startup_scripts/script.js
+++ server/kubejs/startup_scripts/script.js
@@ -1,13 +1,56 @@
-// priority: 0
+WorldgenEvents.add((event) => {
+    const {anchors} = event;
+// Mercury ores
 
-console.info('Hello, World! (You will only see this line once in console, during startup)')
+    event.addOre((ore) => {
+        ore.id = "kubejs:mercurycaloriteore";
+        ore.biomes = [
+            "ad_astra:mercury_deltas",
+            "biomesoplenty:withered_abyss",
+            "biomesoplenty:erupting_inferno",
+            "biomesoplenty:undergrowth",
+            "biomesoplenty:crystalline_chasm",
+            "biomesoplenty:visceral_heap",
+        ];
+        ore.addTarget(
+            "#ad_astra:mercury_ore_replaceables",
+            "ad_astra:deepslate_calorite_ore"
+        );
+        ore
+            .count([10, 15])
+            .squared()
+            .triangleHeight(anchors.aboveBottom(64), anchors.absolute(25));
+        ore.noSurface = 0.6;
+        ore.size = 5;
+    });
 
-StartupEvents.registry('item', event => {
-	// Register new items here
-	// event.create('example_item').displayName('Example Item')
-})
+// Venus ores
 
-StartupEvents.registry('block', event => {
-	// Register new blocks here
-	// event.create('example_block').material('wood').hardness(1.0).displayName('Example Block')
-})
+    event.addOre((ore) => {
+        ore.id = "kubejs:venuscaloriteore";
+        ore.biomes = [
+            "ad_astra:infernal_venus_barrens",
+            "ad_astra:venus_wastelands",
+            "byg:embur_bog",
+            "byg:sythian_torrids",
+            "byg:warped_desert",
+            "byg:quartz_desert",
+            "byg:brimstone_caverns",
+            "byg:weeping_mire",
+            "byg:magma_wastes",
+            "byg:wailing_garth",
+            "byg:crimson_gardens",
+        ];
+        ore.addTarget(
+            "#ad_astra:venus_ore_replaceables",
+            "ad_astra:deepslate_calorite_ore"
+        );
+        ore
+            .count([12, 18])
+            .squared()
+            .triangleHeight(anchors.aboveBottom(64), anchors.absolute(25));
+        ore.noSurface = 0.6;
+        ore.size = 5;
+    });
+});
+
